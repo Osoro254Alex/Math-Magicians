0a196690b2bc53a8176f196ba3181fb2bab15b13
Alex Ogeto <103739438+Osoro254Alex@users.noreply.github.com>
2022-11-09 10:02:37 +0300

    Complete Static Calculator UI

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
+/* eslint-disable */
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.js';
 
 test('renders learn react link', () => {
   render(<App />);
